5eaaae30c054981091311db55b2eedce1856efdd
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-16 15:18:50 +0100

    testing new layout and icons

--- app.vue
+++ app.vue
@@ -2,4 +2,9 @@
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <button class="btn btn-secondary">
+    view Map  
+    <Icon name="tabler:map-pin-pause" />
+
+</button>
 </template>
