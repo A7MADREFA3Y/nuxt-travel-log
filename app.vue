106326d82c3e7a3d45274fcf43cccfdb4a89a0c7
A7MADREFA3Y <147413497+A7MADREFA3Y@users.noreply.github.com>
2025-12-18 14:39:16 +0100

    Tl 11 (#51)
    * chagned the app.vue folder
    
    * added assets with css
    
    * added tailwind to the project
    
    * added recommendations
    
    * update the file to have the plugin
    
    * testing new layout and icons
    
    * installed new dep
    
    * update the packages to have icons
    
    * used the icon in the app to test
    
    * add landing page

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn btn-secondary">
-    view Map  
-    <Icon name="tabler:map-pin-pause" />
-    
-</button>
+  <NuxtLayout>
+      <NuxtPage />
+  </NuxtLayout>
 </template>
