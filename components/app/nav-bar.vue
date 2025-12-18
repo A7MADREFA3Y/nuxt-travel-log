4bfc18baa297e4fb0d4e4ae9f9f7ef39241d9835
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-18 16:20:06 +0100

    add theme toggle

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="navbar bg-primary text-primary-content">
-        <div class="navbar-start">
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
 
-        <NuxtLink to="/" class="btn btn-ghost text-xl">
-            Travle Log
-        </NuxtLink>
-        </div>
-
-        <div class="navbar-end">
-            <a class="btn btn-accent"> 
-            sign In with GitHub
-                <Icon name="tabler:brand-github" size="24" />
-            </a>
-        </div>
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travle Log
+      </NuxtLink>
     </div>
+    <div class="navbar-end">
+      <AppThemeToggle/>
+      <a class="btn btn-accent">
+        sign In with GitHub
+        <Icon name="tabler:brand-github" size="24"/>
+      </a>
+    </div>
+  </div>
 </template>
