8c5ceaa38df5a30a5e9becda6a2880b7aeec4b3d
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-19 08:41:38 +0100

    add try-env-parse and env

--- lib/env.ts
+++ lib/env.ts
@@ -1,10 +1,16 @@
 import { z } from "zod";
 
+import tryParseEnv from "./try-parse-env";
 const EnvSchema = z.object({
     NODE_ENV: z.string(),
 })
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
 
+tryParseEnv(EnvSchema);
+
 // eslint-disable-next-line node/no-process-env
 export default EnvSchema.parse(process.env);
+
+
+// 1:10:45
