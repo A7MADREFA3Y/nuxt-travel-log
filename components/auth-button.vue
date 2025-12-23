ab3a0f0826b133e8ae59714ecbdc054ba1767bc3
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-23 13:41:08 +0100

    logout system with login avatar

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -7,7 +7,23 @@ const authStore = useAuthStore()
 </script>
 
 <template>
-  <button @click="authStore.signIn" :disabled="authStore.loading" class="btn btn-accent" >
+<div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+  <div tabindex="0" role="button" class="btn m-1">
+    <div v-if="authStore.user.image" class="avatar">
+      <div class="w-8 rounded-full">
+        <img :src="authStore.user.image" :alt="authStore.user.name">
+      </div>
+    </div>
+    {{ authStore.user.name }}
+  </div>
+  <ul tabindex="-1" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+    <li><NuxtLink to="/sign-out">
+      <Icon name="tabler:logout-2" size="24" />
+      Sign Out
+    </NuxtLink></li>
+  </ul>
+</div>
+  <button v-else @click="authStore.signIn" :disabled="authStore.loading" class="btn btn-accent" >
     sign In with GitHub
     <span v-if="authStore.loading" class="loading loading-ring loading-md"></span>
     <Icon v-else="authStore.loading" name="tabler:brand-github" size="24"/>
