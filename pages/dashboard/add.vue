ff982a5447a6d2ee455759835d16b44d97b22cd0
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-26 00:15:11 +0100

    add form filed to add location

--- pages/dashboard/add.vue
+++ pages/dashboard/add.vue
@@ -1,61 +1,106 @@
 <script lang="ts" setup>
-import { InsertLocation } from '~/lib/db/schema';
-import { toTypedSchema } from '@vee-validate/zod';
-import { error } from 'better-auth/api';
+import type { FetchError } from "ofetch";
 
-const { handleSubmit, errors } = useForm({
-    validationSchema: toTypedSchema(InsertLocation),
+import { toTypedSchema } from "@vee-validate/zod";
 
+import { InsertLocation } from "~/lib/db/schema";
+
+const router = useRouter();
+const loading = ref(false);
+const submitted = ref(false);
+const submitError = ref("");
+const { handleSubmit, errors, meta, setErrors } = useForm({
+  validationSchema: toTypedSchema(InsertLocation),
 });
 
-const onSubmit = handleSubmit((values) => {
-    console.log(values);
-})
+const onSubmit = handleSubmit(async (values) => {
+  try {
+    submitError.value = "";
+    loading.value = true;
+    await $fetch("/api/locations", {
+      method: "post",
+      body: values,
+    });
+    submitted.value = true;
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+    if (error.data?.data) {
+      setErrors(error.data?.data);
+    }
+    submitError.value = error.statusMessage || "An unknown error occurred.";
+  }
+  loading.value = false;
+});
+
+onBeforeRouteLeave(() => {
+  if (!submitted.value && meta.value.dirty) {
+    // eslint-disable-next-line no-alert
+    const confirm = window.confirm("Are you sure you want to leave? All unsaved changes will be lost.");
+    if (!confirm) {
+      return false;
+    }
+  }
+  return true;
+});
 </script>
 
 <template>
-<div class="container max-w-md mx-auto">
+  <div class="container max-w-md mx-auto">
     <div class="my-4">
-        <h1 class="text-lg">
-            Add Location
-        </h1>
-        <p class="text-sm">
-            A location is a place that you have or will travel to. It can be a city, country, state or point of interest.
-            You can specify the times you visited the location after adding it. 
-        </p>
+      <h1 class="text-lg">
+        Add Location
+      </h1>
+      <p class="text-sm">
+        A location is a place you have traveled or will travel to. It can be a city, country, state or point of interest. You can add specific times you visited this location after adding it.
+      </p>
+    </div>
+    <div v-if="submitError" role="alert" class="alert alert-error">
+      <span>{{ submitError }}</span>
     </div>
     <form class="flex flex-col gap-2" @submit.prevent="onSubmit">
-        
-        <AppFormField label="Name" name="name" :error="errors.name"/>
-        <AppFormField 
-            label="description"
-            name="Description"
-            type="textarea"
-            :error="errors.name"
-        />
-        
-        <AppFormField 
-            label="Latitude"
-            name="lat" 
-            :error="errors.name"
-        />
-
-        <AppFormField 
-            label="Longitude"
-            name="long" 
-            :error="errors.name"
-        />
-        <div class="flex justify-end gap-2">
-            <button type="button" class="btn btn-outline">
-                <Icon name="tabler:arrow-left" size="24"/>        
-                Cancel
-            </button>
-            <button type="submit" class="btn btn-primary">
-                Add
-                <Icon name="tabler:circle-plus-filled" size="24"/>        
-            </button>
-        </div>
+      <AppFormField
+        name="name"
+        label="Name"
+        :error="errors.name"
+        :disabled="loading"
+      />
+      <AppFormField
+        name="description"
+        label="Description"
+        type="textarea"
+        :error="errors.description"
+        :disabled="loading"
+      />
+      <AppFormField
+        name="lat"
+        label="Latitude"
+        :error="errors.lat"
+        :disabled="loading"
+      />
+      <AppFormField
+        name="long"
+        label="Longitude"
+        :error="errors.long"
+        :disabled="loading"
+      />
+      <div class="flex justify-end gap-2">
+        <button
+          :disabled="loading"
+          type="button"
+          class="btn btn-outline"
+          @click="router.back()"
+        >
+          <Icon name="tabler:arrow-left" size="24" />
+          Cancel
+        </button>
+        <button :disabled="loading" type="submit" class="btn btn-primary">
+          Add
+          <span v-if="loading" class="loading loading-ring loading-md" />
+          <Icon v-else name="tabler:circle-plus-filled" size="24" />
+        </button>
+      </div>
     </form>
-</div>
-
+  </div>
 </template>
