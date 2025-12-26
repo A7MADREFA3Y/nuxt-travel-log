ff6e1991b0535ee36fcb5fe79b8bc6f3b295dd3a
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-26 11:50:16 +0100

    add csurf request to the website

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,10 @@ export default defineNuxtConfig({
             '@nuxt/icon',
             '@nuxtjs/color-mode',
             '@pinia/nuxt',
-            '@vee-validate/nuxt'],
+            '@vee-validate/nuxt',
+            'nuxt-csurf'
+          ],
+
   css: ['~/assets/css/main.css'],
   eslint: {
     config: {
