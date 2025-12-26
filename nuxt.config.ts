b1342413b42227e3c43b7228757b85c171462805
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-26 12:00:57 +0100

    Tl 22 (#66)
    * add add page with input fileds and validate it
    
    * add form filed to add location
    
    * add csurf request to the website
    
    * update the signIn/out methods

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,14 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   compatibilityDate: '2025-12-16',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint',
+            '@nuxt/icon',
+            '@nuxtjs/color-mode',
+            '@pinia/nuxt',
+            '@vee-validate/nuxt',
+            'nuxt-csurf'
+          ],
+
   css: ['~/assets/css/main.css'],
   eslint: {
     config: {
