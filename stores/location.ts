6bcc69eddbfb5a64e4bfa4e135188965f3620497
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-27 17:55:35 +0100

    fixed the location 500 server error

--- stores/location.ts
+++ stores/location.ts
@@ -1,33 +1,32 @@
-import { defineStore } from "pinia";
-import { useSidebarStore } from '~/stores/sidebar';
+import { defineStore } from "pinia"
+import { useSidebarStore } from '~/stores/sidebar'
 
-const { data, status, refresh } = useFetch<Array<{ 
+export const useLocationStore = defineStore("useLocationStore", () => {
+  const sidebarStore = useSidebarStore()
+  
+  const { data, status, refresh } = useFetch<Array<{ 
     id: string;
     name: string;
     description: string
- }>>("/api/locations", {
+  }>>("/api/locations", {
     lazy: true,
-});
+  })
 
-export const useLocationStore = defineStore("useLocationStore", () => {
-
-const sidebarStore = useSidebarStore();
-
-watchEffect(() => {
-    if(data.value){
-        sidebarStore.sidebarItems = data.value.map(location => ({
-            id:`location-${location.id}`,
-            label: location.name,
-            icon: "tabler:map-pin-filled",
-            href: "#",
-        }));
+  watchEffect(() => {
+    if (data.value) {
+      sidebarStore.sidebarItems = data.value.map(location => ({
+        id: `location-${location.id}`,
+        label: location.name,
+        icon: "tabler:map-pin-filled",
+        href: "#",
+      }))
     } 
-    sidebarStore.loading = status.value === "pending";    
-});
-    return{
-        locations: data,
-        status,
-        refresh,
-    }
-})
+    sidebarStore.loading = status.value === "pending"    
+  })
 
+  return {
+    locations: data,
+    status,
+    refresh,
+  }
+})
