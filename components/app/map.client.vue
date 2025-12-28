0b8223f99f61fd147eb36f967193ae5a9dec274e
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-28 18:25:45 +0100

    -madd map

---
+++ components/app/map.client.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { CENTRAL_GERMANY } from '~/lib/constants';
+
+
+const colorMode = useColorMode();
+
+const style = computed(() =>
+ colorMode.value === "dark" 
+  ? "/styles/dark.json" 
+  : "https://tiles.openfreemap.org/styles/liberty" ) 
+
+const center = CENTRAL_GERMANY;
+const zoom = 5;
+</script>
+
+
+<template>
+  <MglMap
+    :map-style="style"
+    :center="center"
+    :zoom="zoom"
+  >
+    <MglNavigationControl />
+  </MglMap>
+</template>
+
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,11 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss(),
       ],
+      optimizeDeps:{
+        include: [
+          "maplibre-gl",
+        ]
+      }
   },
     colorMode:{
           dataValue:"theme",
