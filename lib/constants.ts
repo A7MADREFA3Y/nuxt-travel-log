0364d2be51f0cb56fcb1f6c0540a7b1d3ad6121f
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2025-12-28 19:40:43 +0100

    Tl 28 (#72)
    * add points location to the map
    
    * define some stuff for the points on the map
    
    * zoom and some stuff fitting the page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
-export const CENTRAL_GERMANY = [10.4515, 51.1657];
+import type { LngLatLike } from "maplibre-gl";
+
+export const CENTRAL_GERMANY = [10.4515, 51.1657] as LngLatLike;
--- stores/location.ts
+++ stores/location.ts
@@ -1,24 +1,33 @@
 import { defineStore } from "pinia"
 import { useSidebarStore } from '~/stores/sidebar'
+import { useMapStore } from '~/stores/map'
 
 export const useLocationStore = defineStore("useLocationStore", () => {
-  const sidebarStore = useSidebarStore()
-  
+  const sidebarStore = useSidebarStore();
+  const mapStore = useMapStore();  
   const { data, status, refresh } = useFetch<Array<{ 
     id: string;
     name: string;
-    description: string
+    description: string;
+    lat: string;
+    long: string
   }>>("/api/locations", {
     lazy: true,
   })
 
-  watchEffect(() => {
+  effect(() => {
     if (data.value) {
       sidebarStore.sidebarItems = data.value.map(location => ({
         id: `location-${location.id}`,
         label: location.name,
         icon: "tabler:map-pin-filled",
         href: "#",
+      }));
+      mapStore.mapPoints = data.value.map(location => ({
+        id: location.id,
+        label: location.name,
+        lat: location.lat,
+        long: location.long,
       }))
     } 
     sidebarStore.loading = status.value === "pending"    
