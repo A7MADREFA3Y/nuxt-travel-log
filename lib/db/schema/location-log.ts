4e8f4556b8957e9f7aa60435dc2064f62be2ee09
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2026-03-28 22:11:18 +0100

    update imports (#83)

--- lib/db/schema/location-log.ts
+++ lib/db/schema/location-log.ts
@@ -1,4 +1,3 @@
-import { customAlphabet } from "nanoid";
 import { int, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 import { relations } from "drizzle-orm";
@@ -10,18 +9,15 @@ import { DescriptionSchema, LatSchema, LongSchema, NameSchema } from "~/lib/zod-
 import { user } from "./auth";
 import { location } from "./location";
 
-const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
-
 export const locationLog = sqliteTable("locationLog", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
-  slug: text().notNull().unique().$default(() => nanoid()),
   description: text(),
   startedAt: int().notNull(),
   endedAt: int().notNull(),
   lat: real().notNull(),
   long: real().notNull(),
-  locationId: int().notNull().references(() => location.id, { onDelete: "cascade" }),
+  locationId: int().notNull().references(() => location.id),
   userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
@@ -41,7 +37,6 @@ export const InsertLocationLog = createInsertSchema(locationLog, {
   long: LongSchema,
 }).omit({
   id: true,
-  slug: true,
   userId: true,
   locationId: true,
   createdAt: true,
--- stores/auth.ts
+++ stores/auth.ts
@@ -1,51 +1,43 @@
 import {createAuthClient} from "better-auth/vue"
 import {defineStore} from "pinia";
 
-const authClient = createAuthClient()
+const authClient = createAuthClient();
 
 export const useAuthStore = defineStore("useAuthStore", () => {
-    const session = ref<Awaited<ReturnType<typeof authClient.useSession> | null>>(null);
+  const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
 
-    async function init() {
-        const data = await authClient.useSession(useFetch);
-        session.value = data;
-    }
+  async function init() {
+    const data = await authClient.useSession(useFetch);
+    session.value = data;
+  }
 
-    const user = computed(() => session.value?.data?.user);
+  const user = computed(() => session.value?.data?.user);
+  const loading = computed(() => session.value?.isPending);
 
-    const loading = computed(() => session.value?.isPending);
+  async function signIn() {
+    const { csrf } = useCsrf();
+    const headers = new Headers();
+    headers.append("csrf-token", csrf);
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/dashboard",
+      errorCallbackURL: "/error",
+      fetchOptions: {
+        headers,
+      },
+    });
+  }
 
-    async function signIn() {
-        const { csrf } = useCsrf();
-        const headers = new Headers();
-        headers.append("csrf-token", csrf);
-        await authClient.signIn.social({
-            provider: "github",
-            callbackURL: "/dashboard",
-            errorCallbackURL: "/error",
-            fetchOptions: {
-                headers,
-            },
-        });
-    }
+  async function signOut() {
+    await authClient.signOut();
+    navigateTo("/");
+  }
 
-    async function signOut() {
-        const { csrf } = useCsrf();
-        const headers = new Headers();
-        headers.append("csrf-token", csrf);
-        await authClient.signOut({
-            fetchOptions: {
-                headers,
-            },
-        });
-        navigateTo("/");
-    }
-
-    return {
-        init,
-        signIn,
-        signOut,
-        loading,
-        user,
-    }
+  return {
+    init,
+    loading,
+    signIn,
+    signOut,
+    user,
+  };
 });
