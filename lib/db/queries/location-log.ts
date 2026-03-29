6a476141487aa9fcff483cc570f3f66783468e25
A7MADREFA3Y <Ahmadrefai2@gmail.com>
2026-03-29 11:02:57 +0200

    view location log (#86)

--- lib/db/queries/location-log.ts
+++ lib/db/queries/location-log.ts
@@ -1,24 +1,33 @@
-import { customAlphabet } from "nanoid";
-import slugify from "slug";
 import type { InsertLocationLog } from "../schema";
 
 import db from "..";
 import { locationLog } from "../schema";
 
-const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+import { and, eq } from "drizzle-orm";
+
+export async function findLocationLog(
+  id: number,
+  userId: number,
+) {
+  const foundLog = await db.query.locationLog.findFirst({
+    where: and(
+      eq(locationLog.id, id),
+      eq(locationLog.userId, userId),
+    ),
+  });
+
+  return foundLog;
+}
 
 export async function insertLocationLog(
   locationId: number,
   insertable: InsertLocationLog,
   userId: number,
 ) {
-  const slug = `${slugify(insertable.name)}-${nanoid()}`;
-
   const [inserted] = await db.insert(locationLog).values({
     ...insertable,
     locationId,
     userId,
-    slug,
   }).returning();
 
   return inserted;
